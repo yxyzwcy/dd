3d40abb3de5e5c30f98ee88a571e41a42109dd29
yxyzwcy <127171729+yxyzwcy@users.noreply.github.com>
2023-03-07 11:46:21 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,41 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . . #
+        . # . . #
+        . # # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+})
